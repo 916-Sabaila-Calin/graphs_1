05e1a50d833fcddc688640f2d051a86e7c9c2ad6
916-Sabaila-Calin <calin.sabaila@stud.ubbcluj.ro>
2024-03-01 01:06:12 -0800

    done

--- Graph.py
+++ Graph.py
@@ -61,10 +61,10 @@ def export_graph(g, path):
 
 class Graph:
     def __init__(self):
-        self.__vertices = set()
-        self.__outbound_neighbours = dict()
-        self.__inbound_neighbours = dict()
-        self.__costs = dict()
+        self.__vertices = []
+        self.__outbound_neighbours = {}
+        self.__inbound_neighbours = {}
+        self.__costs = {}
 
     def count_vertices(self):
         return len(self.__vertices)
@@ -124,9 +124,9 @@ class Graph:
         if self.is_vertex(vertex):
             raise Exception(f"The vertex {vertex} is already a part of the graph.")
 
-        self.__vertices.add(vertex)
-        self.__outbound_neighbours[vertex] = set()
-        self.__inbound_neighbours[vertex] = set()
+        self.__vertices.append(vertex)
+        self.__outbound_neighbours[vertex] = []
+        self.__inbound_neighbours[vertex] = []
 
     def erase_vertex(self, vertex):
         if not self.is_vertex(vertex):
@@ -152,8 +152,8 @@ class Graph:
         if self.is_edge(from_vertex, to_vertex):
             raise Exception(f"The edge from {from_vertex} to {to_vertex} is already a part of the graph.")
 
-        self.__outbound_neighbours[from_vertex].add(to_vertex)
-        self.__inbound_neighbours[to_vertex].add(from_vertex)
+        self.__outbound_neighbours[from_vertex].append(to_vertex)
+        self.__inbound_neighbours[to_vertex].append(from_vertex)
         self.__costs[(from_vertex, to_vertex)] = cost
 
     def erase_edge(self, from_vertex, to_vertex):
